4b2f19619ac0e1981dcae9cc8f58cfd1dd3695e0
David Cramer <dcramer@gmail.com>
2014-12-04 08:17:55 -0800

    Switch to Notification base

--- sentry_groveio/models.py
+++ sentry_groveio/models.py
@@ -18,14 +18,14 @@ from django.utils import simplejson as json
 logger = logging.getLogger('sentry.plugins.groveio')
 
 
-class GroveIoOptionsForm(forms.Form):
+class GroveIoOptionsForm(notify.NotificationConfigurationForm):
     token = forms.CharField(help_text="Your Grove.io channel token.")
     service_name = forms.CharField(help_text="Name of the service (displayed in the message)", initial='Sentry')
     url = forms.CharField(help_text="(Optional) Service URL for the web client", required=False)
     icon_url = forms.CharField(help_text="(Optional) Icon for the service", required=False)
 
 
-class GroveIoPlugin(Plugin):
+class GroveIoPlugin(notify.NotificationPlugin):
     author = 'Matt Robenolt'
     author_url = 'https://github.com/mattrobenolt'
     description = 'Post new exceptions to a Grove.io room.'
@@ -44,14 +44,11 @@ class GroveIoPlugin(Plugin):
     def is_configured(self, project):
         return all((self.get_option(k, project) for k in ('token', 'service_name')))
 
-    def post_process(self, group, event, is_new, is_sample, **kwargs):
-        if not is_new:
-            return
+    def notify_users(self, group, event, fail_silently=False):
         token = self.get_option('token', event.project)
         service = self.get_option('service_name', event.project)
-        if token and service:
-            message = '[%s] %s: %s' % (event.server_name, event.get_level_display().upper(), event.error().encode('utf-8').split('\n')[0])
-            self.send_payload(token, service, event, group, message)
+        message = '[%s] %s: %s' % (event.server_name, event.get_level_display().upper(), event.error().encode('utf-8').split('\n')[0])
+        self.send_payload(token, service, event, group, message)
 
     def send_payload(self, token, service, event, group, message):
         url = "https://grove.io/api/notice/%s/" % token
